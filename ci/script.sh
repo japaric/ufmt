20fe8ff66f4f7f5295213d317108821d137ff991
Jorge Aparicio <jorge@japaric.io>
2019-05-01 21:32:55 +0200

    only test x86_64

--- ci/script.sh
+++ ci/script.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 main() {
     cargo check --target $TARGET
 
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo test --target $TARGET
+    if [ $TRAVIS_RUST_VERSION = nightly ] && [ $TARGET = x86_64-unknown-linux-gnu ]; then
+        cargo test
     fi
 }
 
