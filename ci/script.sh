fcbbd11ebc66fffb3741ab203535a6535a26fdb7
Jorge Aparicio <jorge@japaric.io>
2019-05-01 21:21:53 +0200

    fix ci/script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 main() {
     cargo check --target $TARGET
 
-    if [ $TRAVIS_RUST_VERSION = nightly]; then
+    if [ $TRAVIS_RUST_VERSION = nightly ]; then
         cargo test --target $TARGET
     fi
 }
