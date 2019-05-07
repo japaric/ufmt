26a77fd51ce3f96fcbd5874b69e17919c3d0c1b6
Jorge Aparicio <jorge@japaric.io>
2019-05-07 11:18:36 +0200

    impl uDebug for String

--- src/impls/std.rs
+++ src/impls/std.rs
@@ -52,14 +52,14 @@ where
     }
 }
 
-// impl uDebug for String {
-//     fn fmt<W>(&self, f: &mut Formatter<'_, W>) -> Result<(), W::Error>
-//     where
-//         W: uWrite,
-//     {
-//         <str as uDebug>::fmt(self, f)
-//     }
-// }
+impl uDebug for String {
+    fn fmt<W>(&self, f: &mut Formatter<'_, W>) -> Result<(), W::Error>
+    where
+        W: uWrite,
+    {
+        <str as uDebug>::fmt(self, f)
+    }
+}
 
 impl uDisplay for String {
     fn fmt<W>(&self, f: &mut Formatter<'_, W>) -> Result<(), W::Error>
